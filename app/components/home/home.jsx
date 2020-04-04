875bab27fec8c97754539d4a2d955bdc59d6855a
tkachuks <serhiytkachuk88@gmail.com>
2020-04-04 17:54:32 +0300

    updated first page(fix focus on input)

--- app/components/home/home.jsx
+++ app/components/home/home.jsx
@@ -1,46 +1,46 @@
-import { default as adressGetService } from 'project-services/adress.service.js'
 import { patchService as fillingPatchService } from 'project-services/filling-up.service.js'
 import qs from 'qs'
 import PropTypes from 'prop-types'
 import './home.styl'
-const { Redirect } = ReactRouterDOM
-let timeout
 class Home extends React.Component {
   state = {
-    addres: config.data.address ? config.data.address : localStorage.getItem('address'),
+    address: config.data.address ? config.data.address : localStorage.getItem('address'),
     email: config.data.email ? config.data.email : localStorage.getItem('email'),
     name: config.data.name ? config.data.name : localStorage.getItem('name'),
-    isViewAdress: false,
+    validEmail: true,
     adress: []
   }
+
   static propTypes = {
     history: PropTypes.object,
     location: PropTypes.object
   }
+
   componentDidMount = () => {
     if (config.isRTL) document.getElementsByTagName('body')[0].style.direction = 'rtl'
     const email = config.data.email ? config.data.email : localStorage.getItem('email')
-    if (email !== null && email !== '') this.changeEmail(email)
-    let query = JSON.stringify(qs.parse(this.props.history.location.search.slice(1)))
+    if (email !== null && email !== '') this.handleCheckEmail(email)
+    const query = JSON.stringify(qs.parse(this.props.history.location.search.slice(1)))
     localStorage.setItem('query', query)
   }
-  changeEmail = e => {
-    let r = /.+@.+\..+/i
-    this.setState({email: e, isValidation: ''})
-    config.data.email = e
-    localStorage.setItem('email', e)
-    if (e !== '') if (r.test(e)) { this.setState({isValidation: 'validationTrue'}) } else { this.setState({isValidation: 'validationFalse'}) }
+
+  handleChangeInput = e => {
+    const name = e.target.name
+    const value = e.target.value
+    this.setState({
+      [name]: value
+    }, () => {
+      config.data[name] = name
+      localStorage.setItem(name, value)
+    })
   }
-  changeAdress = e => {
-    clearTimeout(timeout)
-    this.setState({addres: e})
-    config.data.address = e
-    localStorage.setItem('address', e)
-    if (e.length > 0) {
-      timeout = setTimeout(() => adressGetService(e).then(r => r.json().then(r =>
-        this.setState({isViewAdress: true, adress: r.results}))), config.timeout)
-    } else this.setState({isViewAdress: false})
+
+  handleCheckEmail = () => {
+    const email = this.state.email
+    const pattern = /.+@.+\..+/i
+    pattern.test(email) || email === '' ? this.setState({ validEmail: true }) : this.setState({ validEmail: false })
   }
+
   facebookLogin = () => {
     FB.login(res => {
       if (res.status === 'connected') {
@@ -73,55 +73,65 @@ class Home extends React.Component {
       scope: 'public_profile,email,user_photos,user_birthday,user_location,user_hometown'
     })
   }
+
   continue = () => {
-    let query = JSON.parse(localStorage.getItem('query'))
+    const query = JSON.parse(localStorage.getItem('query'))
     let body = `b=${query.b}&c=${query.c}&name=${this.state.name}&email=${this.state.email}`
-    if (config.address_based) body = body + `&address=${this.state.addres}`
+    if (config.address_based) body = body + `&address=${this.state.address}`
     fillingPatchService(body).then(r => {
       if (r.status === 204) this.props.history.push(config.urls.baseUrl + config.urls.photo)
     })
   }
+
   render () {
+    const { name, email, address, validEmail } = this.state
     return (
       <div id='home'>
-        {!this.props.history.location.search && <Redirect to={{ pathname: config.urls.baseUrl + config.urls.home, search: config.urls.params }} />}
         <div className='bullets'>
           <div className='bullet active' />
           <div className='bullet' />
           <div className='bullet' />
           <div className='bullet' />
         </div>
-        <h1 className='welcome'>{config.translations.welcome}</h1>
-        <h1 className='be_glad'>{config.translations.be_glad}</h1>
-        <h1 className='skip'>{config.translations.skip}</h1>
+        <p className='welcome'>{config.translations.welcome}</p>
+        <p className='be_glad'>{config.translations.be_glad}</p>
+        <p className='skip'>{config.translations.skip}</p>
         <div className='soc_net'>
-          <div className='facebook-wrap' onClick={this.facebookLogin}>
+          <button className='facebook-wrap' onClick={this.facebookLogin}>
             <img src={config.urls.media + 'facebook.png'} />
-            <h1>{config.translations.facebook}</h1>
-          </div>
+            <p>{config.translations.facebook}</p>
+          </button>
         </div>
         <div className='sep'><span>{config.translations.or}</span></div>
-        <h1 className='or_fill'>{config.translations.or_fill}</h1>
+        <p className='or_fill'>{config.translations.or_fill}</p>
         <div className='inputs'>
-          <input type='text' placeholder={config.translations.full_name} value={this.state.name}
-            onChange={e => {
-              this.setState({name: e.target.value})
-              config.data.name = e.target.value
-              localStorage.setItem('name', e.target.value)
-            }} />
-          <span className={this.state.isValidation}>
-            <input type='text' placeholder={config.translations.email} value={this.state.email} onChange={e => this.changeEmail(e.target.value)} />
-          </span>
+          <input
+            type='text'
+            name='name'
+            placeholder={config.translations.full_name}
+            value={name}
+            onChange={this.handleChangeInput}
+          />
+          <input
+            type='text'
+            name='email'
+            className={validEmail ? 'validEmail' : 'notValidEmail'}
+            placeholder={config.translations.email}
+            value={email}
+            onBlur={this.handleCheckEmail}
+            onChange={this.handleChangeInput}
+          />
           {config.address_based &&
-            <input type='text' placeholder={config.translations.adress} value={this.state.addres} onChange={e => this.changeAdress(e.target.value)} />}
-          <div className={this.state.isViewAdress ? 'adress-list-wrap' : 'hidden'}>
-            {this.state.adress.map(i => (
-              <div onClick={() => this.setState({addres: i.formatted_address, isViewAdress: false}, () => { config.data.address = i.formatted_address })}>{i.formatted_address}</div>)
-            )}
-          </div>
+            <input
+              type='text'
+              name='address'
+              placeholder={config.translations.adress}
+              value={address}
+              onChange={this.handleChangeInput}
+            />}
         </div>
         <div className='btn-wrap'>
-          <button onClick={this.continue}>{config.translations.continue}</button>
+          <button onClick={validEmail && this.continue}>{config.translations.continue}</button>
         </div>
       </div>
     )
