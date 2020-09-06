1fff26f929fa86663dda29edb93cbd5fdeadb497
tkachuks <serhiytkachuk88@gmail.com>
2020-09-06 13:41:26 +0300

    upd selecting gender

--- app/components/pages/other-data/components/single-gender/single-gender.jsx
+++ app/components/pages/other-data/components/single-gender/single-gender.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 import './single-gender.styl'
 
-export const GenderItem = ({id, icon, type }) => {
-  return (
-    <div className='gender_item'>
-      <div className='avatar_wrap'>
-        <img src={config.urls.media + icon} />
-      </div>
-      <div className='type_wrap'>
-        <p>{config.translations.other_data.gender[type]}</p>
-        <div className='select' />
-      </div>
+export const GenderItem = ({ id, gender, icon, active_icon, type, onSelectGender }) => (
+  <div className={'gender_item' + (id === gender ? ' selected_gender' : '')} onClick={() => onSelectGender(id)}>
+    <div className='avatar_wrap'>
+      <img src={config.urls.media + (id === gender ? active_icon : icon)} />
     </div>
-  )
-}
+    <div className='type_wrap'>
+      <p>{config.translations.other_data.gender[type]}</p>
+      <div className='select' />
+    </div>
+  </div>
+)
